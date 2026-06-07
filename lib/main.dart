64ea000311385fcfa3b0e088289548f94a2e4af4
abdullahmeen <abdullahamen321@gmail.com>
2026-06-07 21:56:07 +0500

    Text form field

--- lib/main.dart
+++ lib/main.dart
@@ -17,40 +17,52 @@ class MyApp extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                  keyboardType: TextInputType.emailAddress,
-                  cursorColor: Colors.black,
-                  enabled: true,
-                  style: TextStyle(fontSize: 18, color: Colors.black),
-                  decoration: InputDecoration(
-                    filled: false,
-                    prefixIcon: Icon(Icons.email),
-                    fillColor: Colors.grey.withOpacity(0.3),
-                    hintText: 'Email',
-                    hintStyle: TextStyle(
-                      fontSize: 14,
-                      color: Colors.grey.shade600,
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.red, width: 1),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    errorBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.red, width: 1),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.teal, width: 2),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
-                  onChanged: (value) {
-                    print(value);
-                  },
+              ListTile(
+                leading: CircleAvatar(
+                  radius: 70,
+                  backgroundColor: Colors.black,
+                  backgroundImage: NetworkImage('some image.com'),
                 ),
+                title: Text('Abdullah Amin'),
+                subtitle: Text('hey how u doin!'),
+                trailing: Text('10:50'),
+
               ),
+              //listform field
+              // Padding(
+              //   padding: const EdgeInsets.all(8.0),
+              //   child: TextFormField(
+              //     keyboardType: TextInputType.emailAddress,
+              //     cursorColor: Colors.black,
+              //     enabled: true,
+              //     style: TextStyle(fontSize: 18, color: Colors.black),
+              //     decoration: InputDecoration(
+              //       filled: false,
+              //       prefixIcon: Icon(Icons.email),
+              //       fillColor: Colors.grey.withOpacity(0.3),
+              //       hintText: 'Email',
+              //       hintStyle: TextStyle(
+              //         fontSize: 14,
+              //         color: Colors.grey.shade600,
+              //       ),
+              //       enabledBorder: OutlineInputBorder(
+              //         borderSide: BorderSide(color: Colors.red, width: 1),
+              //         borderRadius: BorderRadius.circular(10),
+              //       ),
+              //       errorBorder: OutlineInputBorder(
+              //         borderSide: BorderSide(color: Colors.red, width: 1),
+              //         borderRadius: BorderRadius.circular(10),
+              //       ),
+              //       focusedBorder: OutlineInputBorder(
+              //         borderSide: BorderSide(color: Colors.teal, width: 2),
+              //         borderRadius: BorderRadius.circular(10),
+              //       ),
+              //     ),
+              //     onChanged: (value) {
+              //       print(value);
+              //     },
+              //   ),
+              // ),
               // RIch text and text span
               // RichText(text:TextSpan(
               //   text: "Don't have an account? ",
