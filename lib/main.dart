7ba1ce776cd53c84ed3a02364b5fe2b7d5702488
abdullahmeen <abdullahamen321@gmail.com>
2026-06-07 16:12:06 +0500

    stack Widget LIFO

--- lib/main.dart
+++ lib/main.dart
@@ -17,66 +17,96 @@ class MyApp extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Row(
-                    children: [
-                      Expanded(
-                        flex: 1,
-                        child: Container(
-                        height: 100,
-                        color: Colors.red,
-                        child: Text('container')),
-                      ),
-                      Expanded(
-                        flex: 3,
-                        child: Container(
+                  SizedBox(
+                    // stack widget LIFO
+                    child: Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        Container(
+                          height: 250,
+                          width: 250,
+                          color: Colors.red,
+                        ),
+                        Container(
+                          height: 250,
+                          width: 250,
+                          color: Colors.blue,
+                        ),
+                        Positioned(
+                          top: 50,
+                          left: 50,
+                          child: Container(
                             height: 100,
+                            width: 100,
                             color: Colors.green,
-                            child: Text('container')
+                          ),
+
                         ),
-                      ),
-                      Expanded(
-                        flex: 3,
-                        child: Container(
-                            height: 100,
-                            color: Colors.blue,
-                            child: Text('container')
-                        ),
-                      ),
-
-                    ],
-                  ),
-                  Center(
-                    child: Container(
-                      height: 250,
-                      width: 250,
-
-
-                      decoration: BoxDecoration(
-                        color: Colors.red,
-                        borderRadius: BorderRadius.circular(10),
-                        border: Border.all(color: Colors.black
-                        , width: 3,),
-                        image: DecorationImage(
-                          fit: BoxFit.fill,
-                          image: NetworkImage('https://images.pexels.com/photos/15315565/pexels-photo-15315565.jpeg')
-                        ),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.deepOrange,
-                            blurRadius: 10,
-                            blurStyle: BlurStyle.outer,
-                          )
-                        ]
-
-
-                      ),
-                      child: Center(
-                        child: Text(
-                          'Login',
-                        ),
-                      ),
+                        Text('heyyy!'),
+                      ],
                     ),
                   )
+
+                  // Row(
+                  //   children: [
+                  //     Expanded(
+                  //       flex: 1,
+                  //       child: Container(
+                  //       height: 100,
+                  //       color: Colors.red,
+                  //       child: Text('container')),
+                  //     ),
+                  //     Expanded(
+                  //       flex: 3,
+                  //       child: Container(
+                  //           height: 100,
+                  //           color: Colors.green,
+                  //           child: Text('container')
+                  //       ),
+                  //     ),
+                  //     Expanded(
+                  //       flex: 3,
+                  //       child: Container(
+                  //           height: 100,
+                  //           color: Colors.blue,
+                  //           child: Text('container')
+                  //       ),
+                  //     ),
+                  //
+                  //   ],
+                  // ),
+                  // Center(
+                  //   child: Container(
+                  //     height: 250,
+                  //     width: 250,
+                  //
+                  //
+                  //     decoration: BoxDecoration(
+                  //       color: Colors.red,
+                  //       borderRadius: BorderRadius.circular(10),
+                  //       border: Border.all(color: Colors.black
+                  //       , width: 3,),
+                  //       image: DecorationImage(
+                  //         fit: BoxFit.fill,
+                  //         image: NetworkImage('https://images.pexels.com/photos/15315565/pexels-photo-15315565.jpeg')
+                  //       ),
+                  //       boxShadow: [
+                  //         BoxShadow(
+                  //           color: Colors.deepOrange,
+                  //           blurRadius: 10,
+                  //           blurStyle: BlurStyle.outer,
+                  //         )
+                  //       ]
+                  //
+                  //
+                  //     ),
+                  //     child: Center(
+                  //       child: Text(
+                  //         'Login',
+                  //       ),
+                  //     ),
+                  //   ),
+                  // )
                 ],
               )
           ),
